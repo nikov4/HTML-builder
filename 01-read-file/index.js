55a63d0a5c423c0f959efdf6a1d37245ebb5aae5
Nikolai O <nik@o84.ru>
2024-01-25 16:22:39 +0300

    refactor: change path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-//const txtFile = "W:\\Git\\HTML-builder\\01-read-file\\text.txt";
-//const thisDir = path.dirname(require.main.filename);
-//path.relative('/data/orandea/test/aaa', '/data/orandea/impl/bbb');
-const thisDir = path.relative("W:\\Git\\HTML-builder", "W:\\Git\\HTML-builder\\01-read-file\\");
-const txtFile = thisDir + "\\text.txt";
+//const thisDir = path.relative("W:\\Git\\HTML-builder", "W:\\Git\\HTML-builder\\01-read-file\\");
+//const txtFile = thisDir + "\\text.txt";
+txtFile = path.resolve(__dirname, 'text.txt');
 //console.log(thisDir);
 //console.log(txtFile);
 const readable = new fs.ReadStream(txtFile);
