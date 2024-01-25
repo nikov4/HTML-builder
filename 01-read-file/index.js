8bfb16f408a7ec73f152fe522af2266afe577819
Nikolai O <nik@o84.ru>
2024-01-25 21:12:01 +0300

    fix: clear unnneded comments in task01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,20 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-//const thisDir = path.relative("W:\\Git\\HTML-builder", "W:\\Git\\HTML-builder\\01-read-file\\");
-//const txtFile = thisDir + "\\text.txt";
 txtFile = path.resolve(__dirname, 'text.txt');
-//console.log(thisDir);
-//console.log(txtFile);
 const readable = new fs.ReadStream(txtFile);
 readable.setEncoding('UTF-8');
-/*
-readable.on('readable', () => {
-  let chunk;
-  while (null !== (chunk = readable.read())) {
-    //console.log(`${chunk}`);
-    //console.log(chunk);
-  }
-});
-*/
 const { stdin, stdout } = require('node:process');
 readable.pipe(stdout);
