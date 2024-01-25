3f2a825f8a7337aeeacac4241b7fde8096197158
Nikolai O <nik@o84.ru>
2024-01-25 21:14:23 +0300

    refactor: clear prettier warnings

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-txtFile = path.resolve(__dirname, 'text.txt');
+const txtFile = path.resolve(__dirname, 'text.txt');
 const readable = new fs.ReadStream(txtFile);
 readable.setEncoding('UTF-8');
-const { stdin, stdout } = require('node:process');
+const { stdout } = require('node:process');
 readable.pipe(stdout);
