de8db01a01b1ffb86a52bc99a22be2fbc566cf09
Nikolai O. <nik@o84.ru>
2026-08-12 11:06:27 +0300

    fix: append file instead of creating a new empty each time

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,36 @@
 const fs = require('node:fs');
-const path = require('node:path');
-const txtFile = path.resolve(__dirname, 'text.txt');
-const writable = new fs.WriteStream(txtFile);
-writable.setDefaultEncoding('UTF-8');
+const { join, resolve, extname, basename, parse } = require('node:path');
+const txtFile = resolve(__dirname, 'text.txt');
+const { access, appendFile, writeFile } = require('fs/promises');
 
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
 
-process.stdout.write('Please, type some words here\n');
+// intro message
+process.stdout.write(
+  'Please, type some words here\nPress \"Ctrl+C\" or type \"exit\" to exit\n',
+);
 
+// check file exist
+async function checkFile() {
+  try {
+    await access(txtFile);
+  } catch {
+    writeFile(txtFile, '', 'utf8');
+  }
+}
+checkFile();
+
+// write input
 rl.on('line', (input) => {
   if (input === 'exit') {
     rl.close();
   } else {
-    writable.write(input);
-    writable.write('\n');
+    appendFile(txtFile, input, 'utf8');
+    appendFile(txtFile, '\n', 'utf8');
   }
 });
+
+// farewell message
 process.on('exit', () => process.stdout.write('\nSee you next time!'));
